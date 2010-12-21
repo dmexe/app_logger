ef8afc2b7699a8377e66b3387384ca6a2994e063
Dmitry Galinsky <dima.exe@gmail.com>
2010-12-22 01:20:26 +0300

    initial commit

---
+++ app_logger.gemspec
@@ -0,0 +1,26 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
+Gem::Specification.new do |s|
+    s.name        = %q{app_logger}
+    s.version     = "0.0.1"
+    s.summary     = %q{Application logger}
+    s.description = %q{Application logger}
+
+    s.files        = Dir['[A-Z]*', 'lib/**/*.rb', 'spec/**/*.rb', 'features/**/*', 'rails/**/*']
+    s.require_path = 'lib'
+    s.test_files   = Dir['spec/**/*_spec.rb', 'features/**/*']
+
+    s.has_rdoc         = true
+    s.extra_rdoc_files = ["README.rdoc"]
+    s.rdoc_options = ['--line-numbers', "--main", "README.rdoc"]
+
+    s.authors = ["Dmitry Galinsky"]
+    s.email   = %q{dima.exe@gmail.com}
+    s.homepage = "https://github.com/dima-exe/app_logger"
+
+    s.add_development_dependency('rspec')
+    s.add_development_dependency('rr')
+
+    s.platform = Gem::Platform::RUBY
+    s.rubygems_version = %q{1.2.0}
+end
+
---
+++ lib/app_logger/logger.rb
@@ -0,0 +1,101 @@
+require 'fileutils'
+require 'logger'
+
+module Dima
+	class AppLogger
+		attr_accessor :_options
+
+		def initialize(options = {})
+			self._options = {}
+			self._options[:root] = options[:root]
+			self._options[:env]  = options[:env]
+			self._options[:name] = options[:name] || ENV["APP_LOGGER_NAME"]
+			if defined?(Rails)
+				self._options[:root] ||= Rails.root
+				self._options[:env]  ||= Rails.env
+			end
+			self._options[:env]  ||= "development"
+			self._options[:root] ||= Dir.pwd
+			_expire!
+		end
+
+		def _logger
+			unless @logger
+				@logger = if _options[:env] == "test"
+										_create_test_logger
+									elsif ENV["_"] =~ /irb/ || ENV["APP_LOGGER_STDOUT"]
+										_create_stdout_logger
+									else
+										_create_app_logger
+									end
+			end
+			@logger
+		end
+
+		def _create_app_logger
+			name = _options[:env] + "." + (_options[:name] && "#{_options[:name]}.").to_s + _options[:pid].to_s + ".log"
+			name = File.join(self._options[:root], "log", _date_string, name)
+			_mkdir(_log_file)
+			AppLoggerInstance.new(name)
+		end
+
+		def _create_test_logger
+			name = File.join(_options[:root], "log", "test.log")
+			_mkdir(name)
+			AppLoggerInstance.new(name)
+		end
+
+		def _create_stdout_logger
+			AppStdoutLoggerInstance.new
+		end
+
+		def _expired?
+			tm = Time.now.utc
+			rs = _options[:pid]         != Process.pid || 
+						_options[:time].day   != tm.day      ||
+						_options[:time].month != tm.month    ||
+						_options[:time].year  != tm.year
+			tm = nil
+			rs
+		end
+
+		def _expire!
+			self._options[:pid]  = Process.pid
+			self._options[:time] = Time.now.utc
+			if @logger
+				@logger.close if @logger.respond_to?(:close)
+				@logger = nil
+			end
+		end
+
+		def _date_string
+			_options[:time].strftime("%Y%m%d")
+		end
+
+		def _mkdir(path)
+			FileUtils.mkdir_p(File.dirname(path))
+		end
+
+		def method_missing(method, *args, &block)
+			puts method.to_s
+			_expire! if _expired?
+			_logger.__send__(method, *args, &block)
+		end
+	end
+
+	class AppLoggerInstance < ::Logger
+		def format_message(level, time, progname, msg) 
+			tm = time.utc.strftime("%Y/%m/%d %H:%M:%S") + "." + ("%.03d" % (time.usec / 1000))
+			"#{tm} UTC [#{level.split(//)[0]}] -- #{msg}\n"
+  	end  
+	end
+
+	class AppStdoutLoggerInstance < ::Logger
+		def initialize
+			super(STDOUT)
+		end
+		def format_message(level, time, progname, msg)
+			msg.to_s
+		end
+	end
+end
